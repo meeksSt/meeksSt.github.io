859c320500b2524c69ed61040045d9f8b161c4d0
Maksim Makeev <naysonmaks@gmail.com>
2026-06-25 04:36:04 +0300

    rename classes according to BEM

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ import {
 } from './calculator.js';
 
 const PROPERTY_THRESHOLD = 10;
-const recipesBlock = document.querySelector(".recipes");
+const recipesList = document.querySelector(".recipes__list");
 const allRecipes = generateAllRecipes();
-const hint = document.querySelector(".hint--copied");
+const hint = document.querySelector(".recipe-options__hint");
+const tabs = document.querySelectorAll(".recipes__tab")
+const favoritesList = document.querySelector(".recipes__favorites");
 
 const createRecipeCard = (card) => {
     const properties = Object.entries(card.properties).filter(([key, value]) => value >= PROPERTY_THRESHOLD).map(([key, value]) => key[0].toUpperCase() + key.slice(1)).join('<br>');
@@ -16,7 +18,7 @@ const createRecipeCard = (card) => {
     newCard.setAttribute("data-id", card.id);
 
     newCard.innerHTML = `
-        <div class="recipe--info">                
+        <div class="recipe__info">                
             <fieldset>
                 <legend>Malt</legend>
                 <span>${card.malt}</span>
@@ -32,27 +34,27 @@ const createRecipeCard = (card) => {
             <span>${properties}</span>
         </div>
 
-        <div class="recipe--options">
-            <div class="hint--copied">Copied!</div>
-            <button class="btn--options">
-                <img class="btn--copy" src="/images/copy-to-clipboard.svg" alt="copy to clipboard">
-                <img class="btn--copied" src="/images/copy-to-clipboard-check.svg" alt="copy to clipboard check">
+        <div class="recipe__options recipe-options">
+            <div class="recipe-options__hint recipe-options__hint--disabled">Copied!</div>
+            <button class="button recipe-options__button">
+                <img class="recipe-options__icon recipe-options__icon--copy" src="/images/copy-to-clipboard.svg" alt="copy to clipboard">
+                <img class="recipe-options__icon recipe-options__icon--copied" src="/images/copy-to-clipboard-check.svg" alt="copy to clipboard check">
             </button>
         </div>
     `;
 
-    recipesBlock.appendChild(newCard);
+    recipesList.appendChild(newCard);
 };
 
 const createRecipesList = (items) => {
     [...items].sort((a, b) => a.cntProps - b.cntProps).reverse().forEach(item => createRecipeCard(item));
 };
 
-const clearRecipesList = () => recipesBlock.innerHTML = '';
+const clearRecipesList = () => recipesList.innerHTML = '';
 
 const handleStyleClick = (event) => {
-    document.querySelector(".active--style").classList.remove("active--style");
-    event.target.classList.add("active--style");
+    document.querySelector(".styles__button--active").classList.remove("styles__button--active");
+    event.target.classList.add("styles__button--active");
 
     beerSort = event.target.id;
 
@@ -63,17 +65,40 @@ const handleStyleClick = (event) => {
 
 const handlePropertyClick = (event) => {
     if (beerProperties.includes(event.target.id)) {
-        event.target.classList.remove("active--prop");
+        event.target.classList.remove("properties__button--active");
         beerProperties.splice(beerProperties.indexOf(event.target.id), 1)
     }
     else {
-        event.target.classList.add("active--prop")
+        event.target.classList.add("properties__button--active")
         beerProperties.push(event.target.id) 
     };
 
     recipes = findCorrectBeer(beerSort, beerProperties, allRecipes, PROPERTY_THRESHOLD);
     clearRecipesList();
     createRecipesList(recipes);
+};
+
+const handleTabClick = (event) => {
+    tabs.forEach(tab => {
+        if (tab.classList.contains("recipes__tab--active")) {
+            tab.classList.remove("recipes__tab--active");
+        };
+    });
+
+    if (!event.target.classList.contains("recipes__tab--active")) {
+        event.target.classList.add("recipes__tab--active");
+    };
+
+    if (event.target.dataset.tab == "favorites") {
+        favoritesList.classList.add("recipes__favorites--active");
+        recipesList.classList.remove("recipes__list--active");
+    };
+
+    if (event.target.dataset.tab == "recipes") {
+        recipesList.classList.add("recipes__list--active");
+        favoritesList.classList.remove("recipes__favorites--active");
+    };
+        
 };
 
 const getRecipeText = (recipe) => `
@@ -97,12 +122,13 @@ const copyTextToClipboard = (event) => {
 const handleCopyClick = (event) => {
     copyTextToClipboard(event);
 
-    event.target.classList.add('copied');
-    event.target.previousElementSibling.classList.add('enable');
+    console.log(event.target)
+    event.target.classList.add('recipe-options__button--copied');
+    event.target.previousElementSibling.classList.remove('recipe-options__hint--disabled');
 
     setTimeout(() => {
-        event.target.classList.remove('copied');
-        event.target.previousElementSibling.classList.remove('enable');
+        event.target.classList.remove('recipe-options__button--copied');
+        event.target.previousElementSibling.classList.add('recipe-options__hint--disabled');
     }, 2000);
 };
 
@@ -113,21 +139,25 @@ let recipes = findCorrectBeer(beerSort, beerProperties, allRecipes, PROPERTY_THR
 createRecipesList(recipes);
 
 document.addEventListener("click", (event) => {
-    if (event.target.classList.contains("btn") && !event.target.classList.contains("active--style")) {
-        handleStyleClick(event);
-    };
+    if (event.target.classList.contains("styles__button") && !event.target.classList.contains("styles__button--active")) handleStyleClick(event);
 
-    if (event.target.classList.contains("btn-small")) {
-        handlePropertyClick(event);
-    };
+    if (event.target.classList.contains("properties__button")) handlePropertyClick(event);
 
-    if (event.target.classList.contains("btn--options") && !event.target.classList.contains("copied")) {
-        handleCopyClick(event);
-    };
+    if (event.target.classList.contains("recipe-options__button") && event.target.previousElementSibling.classList.contains("recipe-options__hint--disabled")) handleCopyClick(event);
 
-    if (document.querySelectorAll(".recipe").length == 0) {
-        recipesBlock.innerHTML = "No recipes found..";
-        recipesBlock.classList.add("empty");
+    if (event.target.classList.contains("recipes__tab")) handleTabClick(event);
+
+    if (recipesList.children.length == 0) {
+        recipesList.innerHTML = "No recipes found..";
+        recipesList.classList.add("recipes__list--empty");
+
     }
-    else recipesBlock.classList.remove("empty");
+    else recipesList.classList.remove("recipes__list--empty");
+
+    if (favoritesList.children.length == 0) {
+        favoritesList.innerHTML = "No recipes found..";
+        favoritesList.classList.add("recipes__favorites--empty");
+
+    }
+    else favoritesList.classList.remove("recipes__favorites--empty");
 });
